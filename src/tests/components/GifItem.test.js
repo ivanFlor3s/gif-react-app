3f89b0bce46b9b32cbcbc8068c2e9db9546d4335
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-01-01 15:12:50 -0300

    Agrego pruebas propiedades imagen + h4 title

--- src/tests/components/GifItem.test.js
+++ src/tests/components/GifItem.test.js
@@ -4,9 +4,24 @@ import { GifItem } from "../../components/GifItem";
 
 describe('Pruebas en GifItem component', () => {
 
+    
+    const [title, url] = ['Mi titulo', 'https://esto+es+una+url.jpg']
+    const wrapper = shallow(<GifItem title={title} img={url}></GifItem>)
+
+
     test('Debe mostrar el componente correctamente', () => {
-        const [title, url] = ['Mi titulo', 'https://esto+es+una+url.jpg']
-        const wrapper = shallow(<GifItem title={title} img={url}></GifItem>)
         expect(wrapper).toMatchSnapshot()
+    })
+
+    test('Debe tener un h4 en el titulo', () => {
+        const h3InDoc = wrapper.find('h4')
+        expect(h3InDoc.text().trim()).toBe(title.trim())
+    })
+
+    test('La imagen debe tener un src igual al url y alt correcto', () => {
+        const imgInDoc = wrapper.find('img')
+        expect( imgInDoc.props().src).toBe(url)
+        //expect( imgInDoc.prop('src')).toBe(url)
+        expect(imgInDoc.prop('alt')).toBe(title)
     })
 })
